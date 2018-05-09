7e5d430e69445d10325e34555477b1e3cf13ba22
Your Name <binojishara21@gmail.com>
2018-05-09 23:43:21 +0530

    4th commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,14 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
+     
     <body>
-   @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+
+            @yield('content')
+        </div>
+
 
     </body>
 </html>
